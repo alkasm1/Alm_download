913ed348010bce5ba81ea8b2ba75a55305f47422
رأفت عمر <shamwebs1@gmail.com>
2026-05-23 18:59:40 +0300

    Create signal.js

---
+++ alm_download/signaling_client/signal.js
@@ -0,0 +1,23 @@
+export class SignalClient {
+  constructor(url) {
+    this.ws = new WebSocket(url);
+    this.handlers = {};
+  }
+
+  on(type, fn) {
+    this.handlers[type] = fn;
+  }
+
+  send(to, payload) {
+    this.ws.send(JSON.stringify({ to, ...payload }));
+  }
+
+  bind() {
+    this.ws.onmessage = (ev) => {
+      const data = JSON.parse(ev.data);
+      if (this.handlers[data.type]) {
+        this.handlers[data.type](data);
+      }
+    };
+  }
+}
