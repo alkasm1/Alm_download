d8d5f0feea44bc844d56f5a3ea611200232f8af2
رأفت عمر <shamwebs1@gmail.com>
2026-05-23 19:08:17 +0300

    Create frames.ts

---
+++ alm_download/alm_core/frames.ts
@@ -0,0 +1,19 @@
+export class ALMFrame {
+  constructor(type, payload) {
+    this.type = type;
+    this.payload = payload;
+    this.timestamp = Date.now();
+  }
+
+  toJSON() {
+    return {
+      type: this.type,
+      payload: this.payload,
+      timestamp: this.timestamp
+    };
+  }
+
+  static fromJSON(obj) {
+    return new ALMFrame(obj.type, obj.payload);
+  }
+}
