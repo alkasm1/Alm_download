689492008bb7c39aff60360bdbe7bb5d4ecd30f5
رأفت عمر <shamwebs1@gmail.com>
2026-05-23 18:59:01 +0300

    Create clients.js

---
+++ alm_download/signaling_server/clients.js
@@ -0,0 +1,13 @@
+export const clients = new Map();
+
+export function addClient(id, ws) {
+  clients.set(id, ws);
+}
+
+export function removeClient(id) {
+  clients.delete(id);
+}
+
+export function getClient(id) {
+  return clients.get(id);
+}
