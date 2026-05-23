ae0611490b43b093894511fbac02b2ea661595f1
رأفت عمر <shamwebs1@gmail.com>
2026-05-23 19:23:56 +0300

    Create events.js

---
+++ alm_download/signaling_client/events.js
@@ -0,0 +1,9 @@
+/* path: alm_download/signaling_client/events.js */
+
+export const SignalEvents = {
+  WELCOME: "welcome",
+  OFFER: "offer",
+  ANSWER: "answer",
+  ICE: "ice",
+  REQUEST: "request"
+};
