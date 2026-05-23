da4da6a73b3f1373a6196ba3f5228cde78ff28ab
رأفت عمر <shamwebs1@gmail.com>
2026-05-23 19:28:31 +0300

    Create integrity.js

---
+++ alm_download/smart_downloader/integrity.js
@@ -0,0 +1,9 @@
+/* path: alm_download/smart_downloader/integrity.js */
+
+export function crc32(buf) {
+  let crc = 0 ^ -1;
+  for (let i = 0; i < buf.length; i++) {
+    crc = (crc >>> 8) ^ table[(crc ^ buf[i]) & 0xff];
+  }
+  return (crc ^ -1) >>> 0;
+}
