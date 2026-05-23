86e6a5fdaa64808db003ed5815b2c273bbdbf82a
رأفت عمر <shamwebs1@gmail.com>
2026-05-23 19:10:07 +0300

    Create events.ts

---
+++ alm_download/alm_core/events.ts
@@ -0,0 +1,15 @@
+export class ALMEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, fn) {
+    if (!this.events[event]) this.events[event] = [];
+    this.events[event].push(fn);
+  }
+
+  emit(event, data) {
+    if (!this.events[event]) return;
+    for (const fn of this.events[event]) fn(data);
+  }
+}
