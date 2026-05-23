c573f3193bb986fb754e692a698e247259ef757c
رأفت عمر <shamwebs1@gmail.com>
2026-05-23 20:53:18 +0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-/* path: alm_download/ui_demo/app.js */
-
-document.getElementById("start").onclick = () => {
-  log("Starting session...");
-};
-
-function log(t) {
-  document.getElementById("log").textContent += t + "\n";
-}
